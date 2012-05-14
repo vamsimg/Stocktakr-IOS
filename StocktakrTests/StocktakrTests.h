360dd5aa006e8036259ce3394f54392117043cb2
Sherman Lo <shermanl@lilliputlabs.com>
2012-05-14 16:01:40 +1000

    Initial Commit

---
+++ StocktakrTests/StocktakrTests.h
@@ -0,0 +1,13 @@
+//
+//  StocktakrTests.h
+//  StocktakrTests
+//
+//  Created by Sherman Lo on 14/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface StocktakrTests : SenTestCase
+
+@end
---
+++ StocktakrTests/StocktakrTests.m
@@ -0,0 +1,32 @@
+//
+//  StocktakrTests.m
+//  StocktakrTests
+//
+//  Created by Sherman Lo on 14/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "StocktakrTests.h"
+
+@implementation StocktakrTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in StocktakrTests");
+}
+
+@end
